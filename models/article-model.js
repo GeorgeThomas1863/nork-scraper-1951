0cf13eb37bd51adfb5c351ee9e45f51bae8a01d2
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-17 21:06:01 -0400

    dev

--- models/article-model.js
+++ models/article-model.js
@@ -2,7 +2,7 @@ import { JSDOM } from "jsdom";
 
 import CONFIG from "../config/scrape-config.js";
 import dbModel from "./db-model.js";
-import Pic from "./pic-model.js";
+// import Pic from "./pic-model.js";
 import UTIL from "./util-model.js";
 
 /**
