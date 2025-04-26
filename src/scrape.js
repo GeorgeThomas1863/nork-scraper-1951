5dc3f93221759849e47684e23193c5ea77b4e744
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-25 20:01:17 -0400

    shockingly still fucked

--- src/scrape.js
+++ src/scrape.js
@@ -46,7 +46,7 @@ export const scrapeNewContent = async (type) => {
   const listModel = new KCNA({ url: CONFIG[newListInputObj.param] });
   const newListHTML = await listModel.getHTML();
   //runs function based on type (using map.func)
-  const listArray = await [ballfucker](newListHTML);
+  const listArray = await newListInputObj[ballfucker](newListHTML);
   console.log(listArray);
 
   // //get content
