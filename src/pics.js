6997b219ac1225547f7b1bf804c6e7e300a55229
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-26 15:27:04 -0400

    bugs

--- src/pics.js
+++ src/pics.js
@@ -82,6 +82,9 @@ export const downloadNewPicsFS = async (inputArray) => {
   // const downloadArray = await inputModel.getMediaToScrapeFS();
   // console.log("DOWNLOADING " + downloadArray.length + " FUCKING PICS");
 
+  console.log("PIC INPUT ARRAY")
+  console.log(inputArray?.length)
+
   //easier to do all in model
   const picModel = new Pic({ inputArray: inputArray });
   const downloadPicDataArray = await picModel.downloadPicArray();
