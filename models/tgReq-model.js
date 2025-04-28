b3eb1b470f5f549373d6bce3398559770c59714b
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-04-28 18:43:23 -0400

    combining tg models

--- models/tgReq-model.js
+++ models/tgReq-model.js
@@ -14,8 +14,23 @@ import axios from "axios";
 import tokenArray from "../config/tg-bot.js";
 
 class TgReq {
+  static tokenIndex = 0;
+
   constructor(dataObject) {
     this.dataObject = dataObject;
+  }
+
+  async checkToken(data) {
+    //429 bot fucked error
+    if (!data || (data && data.ok) || (data && !data.ok && data.error_code !== 429)) return null;
+
+    console.log("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+
+    TgAPI.tokenIndex++;
+    if (TgAPI.tokenIndex > 11) TgAPI.tokenIndex = 0;
+
+    console.log("GOT 429 ERROR, TRYING NEW FUCKING BOT. TOKEN INDEX: " + TgAPI.tokenIndex);
+    return TgAPI.tokenIndex;
   }
 
   /**
@@ -89,65 +104,125 @@ class TgReq {
     }
   }
 
-  //PROB NOT NECESSARY
-  /**
-   * TG sendMessage API, sends message chunk to TG (chunk before using) with auto token rotation
-   * @function sendMessageChunkTG
-   * @param {Object} params - Message parameters
-   * @returns {Promise<Object>} Response data from Telegram API
-   */
-  async sendMessageChunkTG(tokenIndex) {
-    const tgModel = new TgReq(this.dataObject);
 
-    //check token
-    let data = await tgModel.tgPost("sendMessage", tokenIndex);
-    const checkData = this.checkToken(data);
-    if (checkData) data = await tgModel.tgPost("sendMessage", tokenIndex); //run again
+  //-----------------------------
 
-    return data;
-  }
 
-  //MIGHT WANT TO CHANGE HOW BELOW HAPPENS
-  /**
-   * TG editMessageCaption API; edits the caption of a previously pic / message
-   * @function editCaptionTG
-   * @param {Object} inputObj - Response object from a previous sendPhoto API call, custom caption
-   * @returns {Promise<Object>} Response data from Telegram API
-   */
-  async editCaptionTG(caption, tokenIndex) {
-    //build params
-    const params = {
-      chat_id: this.dataObject.result.chat.id,
-      message_id: this.dataObject.result.message_id,
-      caption: caption,
-    };
-
-    const tgModel = new TgReq(params);
-
-    let data = await tgModel.tgPost("editMessageCaption", tokenIndex);
-    const checkData = this.checkToken(data);
-    if (checkData) data = await tgModel.tgPost("editMessageCaption", tokenIndex); //if fucked run again
-
-    return data;
-  }
-
-  // //PROB NOT NECESSARY
   // /**
-  //  * TG sendPhoto API, posts images to TG channel / user, with auto token rotation
-  //  * @function uploadPicsTG
-  //  * @param {Object} params - Upload parameters
+  //  * TG sendMessage API, sends message chunk to TG (chunk before using) with auto token rotation
+  //  * @function sendMessageChunkTG
+  //  * @param {Object} params - Message parameters
   //  * @returns {Promise<Object>} Response data from Telegram API
   //  */
-  // async uploadPicsTG(tokenIndex) {
+  // async sendMessageChunkTG(tokenIndex) {
   //   const tgModel = new TgReq(this.dataObject);
 
   //   //check token
-  //   let data = await tgModel.tgPicFS(tokenIndex);
+  //   let data = await tgModel.tgPost("sendMessage", tokenIndex);
   //   const checkData = this.checkToken(data);
-  //   if (checkData) data = await tgModel.tgPicFS(tokenIndex); //run again
+  //   if (checkData) data = await tgModel.tgPost("sendMessage", tokenIndex); //run again
 
   //   return data;
   // }
+
+  // //MIGHT WANT TO CHANGE HOW BELOW HAPPENS
+  // /**
+  //  * TG editMessageCaption API; edits the caption of a previously pic / message
+  //  * @function editCaptionTG
+  //  * @param {Object} inputObj - Response object from a previous sendPhoto API call, custom caption
+  //  * @returns {Promise<Object>} Response data from Telegram API
+  //  */
+  // async editCaptionTG(caption, tokenIndex) {
+  //   //build params
+  //   const params = {
+  //     chat_id: this.dataObject.result.chat.id,
+  //     message_id: this.dataObject.result.message_id,
+  //     caption: caption,
+  //   };
+
+  //   const tgModel = new TgReq(params);
+
+  //   let data = await tgModel.tgPost("editMessageCaption", tokenIndex);
+  //   const checkData = this.checkToken(data);
+  //   if (checkData) data = await tgModel.tgPost("editMessageCaption", tokenIndex); //if fucked run again
+
+  //   return data;
+  // }
+
+  //--------------------------
+
+  async postArticleTitleTG() {
+    const { inputObj } = this.dataObject;
+
+    if (!inputObj) return null;
+
+    const { title, date } = inputObj;
+    const titleText = title + "\n" + "<i>" + date + "</i>";
+
+    const params = {
+      chat_id: CONFIG.tgUploadId,
+      text: titleText,
+      parse_mode: "HTML",
+    };
+
+    const postObj = {
+      command: "sendMessage",
+      params: params,
+    };
+
+    const tgModel = new TgReq({ inputObj: postObj });
+    let data = await tgModel.tgPost(TgReq.tokenIndex);
+
+    //check token
+    const checkData = await this.checkToken(data);
+    if (checkData) {
+      data = await tgModel.tgPost(TgAPI.tokenIndex);
+    }
+
+    return data;
+  }
+
+  async postPicTG() {
+    const { inputObj } = this.dataObject;
+    const { savePath } = inputObj;
+
+    const params = {
+      chatId: CONFIG.tgUploadId,
+      picPath: savePath,
+    };
+
+    const tgModel = new TgReq(params);
+    let data = await tgModel.tgPicFS(TgAPI.tokenIndex);
+
+    //check token
+    const checkData = await this.checkToken(data);
+
+    if (checkData) {
+      console.log("AHHHHHHHHHHHHHHHHHHHHH");
+      data = await tgModel.tgPicFS(TgAPI.tokenIndex);
+      console.log(TgAPI.tokenIndex);
+    }
+
+    // console.log("DATA HERE");
+    // console.log(data);
+
+    // if (!data) return null;
+
+    //EDIT PIC CAPTION
+
+    //STORE PIC AS UPLOADED
+
+    //store pic Posted
+    const storeObj = { ...inputObj, ...data.result };
+    const storeModel = new dbModel(storeObj, CONFIG.picsUploaded);
+    await storeModel.storeUniqueURL();
+
+    return storeObj;
+  }
+
+  // export const postPicTG = async (inputObj) => {
+
+  // };
 }
 
 export default TgReq;
