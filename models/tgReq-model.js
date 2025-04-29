7504c73d19f81287873fc2a510f57f319242d25d
George H. Thomas <georgebushdontcare@gmail.com>
2025-04-28 21:37:10 -0400

    dev

--- models/tgReq-model.js
+++ models/tgReq-model.js
@@ -166,7 +166,7 @@ class TgReq {
     if (!inputObj) return null;
 
     const { title, date } = inputObj;
-    const titleText = "!!!--------------" + "\n\n" + title + "\n" + "<i>" + date + "</i>";
+    const titleText = "--------------" + "\n\n" + "<b>" + title + "</b>" + "\n" + "<i>" + date + "</i>" + "\n\n" + "--------------";
 
     const params = {
       chat_id: CONFIG.tgUploadId,
