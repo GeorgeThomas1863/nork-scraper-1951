76fecb1cda7cb5f53a601417a7ea860b2a7adf50
George H. Thomas <georgebushdontcare@gmail.com>
2025-05-11 11:50:22 -0400

    still bugs

--- src/vids.js
+++ src/vids.js
@@ -127,6 +127,9 @@ export const downloadVidPageArray = async (inputArray) => {
 //UPLOAD SHIT
 
 export const uploadVidPageArrayTG = async (inputArray) => {
+  console.log("!!!!!!!UPLOAD VID PAGE ARRAY");
+  console.log(inputArray);
+
   const sortModel = new UTIL({ inputArray: inputArray });
   const sortArray = await sortModel.sortArrayByDate();
   if (!sortArray || !sortArray.length) return null;
