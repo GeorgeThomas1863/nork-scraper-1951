748d1222c792cbccfbc1a0abc0e938faad3095ca
George Thomas <fake@email.org>
2025-06-29 16:28:22 -0400

    redoing scraper to use state

--- src/scrape-command.js
+++ src/scrape-command.js
@@ -1,56 +1,24 @@
 import Log from "../models/log-model.js";
+import { textStrMap } from "../config/map-scrape.js";
+import { scrapeState, updateScrapeStateByCommand } from "./scrape-state.js";
 import { scrapeNewKCNA, scrapeAllKCNA, scrapeUrlKCNA } from "./scrape-control.js";
-import { setContinueScrape, scrapeActive, scrapeId, setScrapeId } from "./scrape-util.js";
+import { setContinueScrape } from "./scrape-util.js";
 import { startScheduler, stopScheduler } from "./scrape-scheduler.js";
 
 //REFACTOR / break into multiple funcitons
 export const parseAdminCommand = async (inputParams) => {
-  const { commandType, intervalMinutes } = inputParams;
+  const { commandType } = inputParams;
   if (!commandType) return null;
-  const returnObj = {};
+  // const { scrapeId, scrapeActive, schedulerActive, textStr, runScrape } = scrapeState;
 
-  //if stop scrape
-  if (commandType === "admin-stop-scrape") {
-    await setContinueScrape(false);
+  console.log("SCRAPE STATE BEFORE UPDATE: ", scrapeState);
 
-    returnObj.textStr = "STOPPING SCRAPE";
-    returnObj.scrapeId = scrapeId;
-    returnObj.runScrape = null;
+  const updateStateData = await updateScrapeStateByCommand(commandType);
 
-    console.log(returnObj.textStr);
-    return returnObj;
-  }
+  console.log("SCRAPE STATE AFTER UPDATE: ", scrapeState);
 
-  if (commandType === "admin-stop-scheduler") {
-    await stopScheduler();
-    returnObj.textStr = "STOPPING SCHEDULER";
-    returnObj.scrapeId = scrapeId;
-    returnObj.runScrape = null;
-
-    console.log(returnObj.textStr);
-    return returnObj;
-  }
-
-  //if reset auto
-  if (commandType === "admin-start-scheduler") {
-    await startScheduler();
-    returnObj.textStr = "STARTING SCHEDULER";
-    returnObj.scrapeId = scrapeId;
-    returnObj.runScrape = null;
-
-    console.log(returnObj.textStr);
-    return returnObj;
-  }
-
-  //deal with later
-  //if just getting status, return same as scrape active
-  // if (commandType === "admin-scrape-status") {
-  //   const schedulerStatus = getSchedulerStatus();
-  //   returnObj.textStr = `SCRAPE STATUS: ${scrapeActive ? "ACTIVE" : "INACTIVE"}. SCHEDULER: ${schedulerStatus.isActive ? `ACTIVE (${schedulerStatus.interval}min)` : "INACTIVE"}`;
-  //   returnObj.scrapeId = scrapeId;
-  //   returnObj.runScrape = null;
-  //   return returnObj;
-  // }
+  //!!! HERE
+  //
 
   //if already scraping
   if (scrapeActive) {
@@ -75,6 +43,93 @@ export const parseAdminCommand = async (inputParams) => {
   returnObj.runScrape = true;
 
   return returnObj;
+
+  // const returnObj = {};
+  // switch (commandType) {
+  //   case "admin-scrape-status":
+  //     scrapeState.textStr = `SCRAPE STATUS: ${scrapeActive ? "ACTIVE" : "INACTIVE"}. SCHEDULER: ${schedulerActive ? "ACTIVE" : "INACTIVE"}`;
+  //     scrapeState.runScrape = null;
+  //     return returnObj;
+
+  //   case "admin-stop-scrape":
+  //     scrapeState.scrapeActive = false;
+  //     scrapeState.textStr = "STOPPING SCRAPE";
+  //     scrapeState.runScrape = null;
+  //     return returnObj;
+
+  //   case "admin-stop-scheduler":
+  //     scrapeState.schedulerActive = false;
+  //     scrapeState.textStr = "STOPPING SCHEDULER";
+  //     scrapeState.runScrape = null;
+  //     return returnObj;
+
+  // case "admin-start-scrape":
+  //   // await startScheduler();
+  //   // returnObj.textStr = "STARTING SCHEDULER";
+  //   // returnObj.scrapeId = scrapeId;
+  //   // returnObj.runScrape = null;
+
+  //   scrapeActive = true;
+  //   textStr = "STARTING SCRAPE";
+  //   runScrape = true;
+
+  //   return returnObj;
+
+  // case "admin-start-scheduler":
+  //   // await startScheduler();
+  //   // returnObj.textStr = "STARTING SCHEDULER";
+  //   // returnObj.scrapeId = scrapeId;
+  //   // returnObj.runScrape = null;
+
+  //   schedulerActive = true;
+  //   textStr = "STARTING SCHEDULER";
+  //   runScrape = true;
+
+  //   return returnObj;
+  // }
+
+  //if stop scrape
+  // if (commandType === "admin-stop-scrape") {
+  //   await setContinueScrape(false);
+
+  //   returnObj.textStr = "STOPPING SCRAPE";
+  //   returnObj.scrapeId = scrapeId;
+  //   returnObj.runScrape = null;
+
+  //   console.log(returnObj.textStr);
+  //   return returnObj;
+  // }
+
+  // if (commandType === "admin-stop-scheduler") {
+  //   await stopScheduler();
+  //   returnObj.textStr = "STOPPING SCHEDULER";
+  //   returnObj.scrapeId = scrapeId;
+  //   returnObj.runScrape = null;
+
+  //   console.log(returnObj.textStr);
+  //   return returnObj;
+  // }
+
+  //if reset auto
+  // if (commandType === "admin-start-scheduler") {
+  //   await startScheduler();
+  //   returnObj.textStr = "STARTING SCHEDULER";
+  //   returnObj.scrapeId = scrapeId;
+  //   returnObj.runScrape = null;
+
+  //   console.log(returnObj.textStr);
+  //   return returnObj;
+  // }
+
+  //deal with later
+  //if just getting status, return same as scrape active
+  // if (commandType === "admin-scrape-status") {
+  //   const schedulerStatus = getSchedulerStatus();
+  //   returnObj.textStr = `SCRAPE STATUS: ${scrapeActive ? "ACTIVE" : "INACTIVE"}. SCHEDULER: ${schedulerStatus.isActive ? `ACTIVE (${schedulerStatus.interval}min)` : "INACTIVE"}`;
+  //   returnObj.scrapeId = scrapeId;
+  //   returnObj.runScrape = null;
+  //   return returnObj;
+  // }
 };
 
 export const runScrapeCommand = async (inputParams) => {
--- src/scrape-util.js
+++ src/scrape-util.js
@@ -3,38 +3,6 @@ import path from "path";
 import dbModel from "../models/db-model.js";
 import { deleteItemsMap } from "../config/map-scrape.js";
 
-//STOP SCRAPE CHECK
-export let continueScrape = true;
-
-// Function to update the state
-export const setContinueScrape = async (inputValue) => {
-  continueScrape = inputValue;
-  console.log("CONTINUE SCRAPE STATUS CHANGED TO: " + String(inputValue).toUpperCase());
-  return continueScrape;
-};
-
-//---------------
-
-//SCRAPE ACTIVE CHECK (returns null if active, prevents multiple commands)s
-export let scrapeActive = false;
-
-export const setScrapeActive = async (inputValue) => {
-  scrapeActive = inputValue;
-  console.log("SCRAPE ACTIVE STATUS CHANGED TO: " + String(inputValue).toUpperCase());
-  return scrapeActive;
-};
-
-//--------------
-
-//SCRAPE ID
-export let scrapeId = 0;
-
-export const setScrapeId = async (inputValue) => {
-  scrapeId = inputValue;
-  console.log("SCRAPE ID SET TO: " + scrapeId);
-  return scrapeId;
-};
-
 //------------------------------
 
 //size of item in db
