bda3f62aaf5694a599759b8affbf954a7c9fbf5c
George Thomas <fake@email.org>
2025-06-29 15:12:14 -0400

    still trying to make admin a thing

--- controllers/api.js
+++ controllers/api.js
@@ -4,6 +4,9 @@ import { parseAdminCommand, runScrapeCommand } from "../src/scrape-command.js";
 export const apiRoute = async (req, res) => {
   const inputParams = req.body;
 
+  console.log("!!!!API ROUTE");
+  console.log(inputParams);
+
   //gets scrapeId + message, returns it
   const data = await parseAdminCommand(inputParams);
   res.json(data);
