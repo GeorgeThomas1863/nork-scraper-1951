8f7db2070443026fd1b38656fd0b00167131a046
George H. Thomas <georgebushdontcare@gmail.com>
2025-06-30 08:47:37 -0400

    dev

--- src/scrape-scheduler.js
+++ src/scrape-scheduler.js
@@ -3,23 +3,27 @@ import { scrapeNewKCNA } from "./scrape-control.js";
 // import { scrapeActive } from "./scrape-util.js";
 import { scrapeState } from "./scrape-state.js";
 
-//FIX 
+//FIX [see if can move schedulerInterval to scrapeState]
 
 let schedulerInterval = null;
-let isSchedulerActive = false;
 
 export const startScheduler = async () => {
   const { scrapeDelay } = CONFIG;
+  const { schedulerActive, scrapeActive } = scrapeState;
 
   // If scheduler is already running, stop it first
-  if (isSchedulerActive) {
-    await stopScheduler();
+  if (schedulerActive) {
+    scrapeState.schedulerActive = false;
+    if (schedulerInterval) {
+      clearInterval(schedulerInterval);
+      schedulerInterval = null;
+    }
   }
 
   // Start the scheduler
   schedulerInterval = setInterval(async () => {
     // Only run if not already scraping
-    if (!scrapeState.scrapeActive) {
+    if (!scrapeActive) {
       try {
         await scrapeNewKCNA();
       } catch (error) {
@@ -28,22 +32,22 @@ export const startScheduler = async () => {
     }
   }, scrapeDelay);
 
-  isSchedulerActive = true;
+  scrapeState.schedulerActive = true;
   return true;
 };
 
-export const stopScheduler = async () => {
-  if (schedulerInterval) {
-    clearInterval(schedulerInterval);
-    schedulerInterval = null;
-  }
-  isSchedulerActive = false;
-  return true;
-};
+// export const stopScheduler = async () => {
+//   if (schedulerInterval) {
+//     clearInterval(schedulerInterval);
+//     schedulerInterval = null;
+//   }
+//   scrapeState.schedulerActive = false;
+//   return true;
+// };
 
-export const getSchedulerStatus = () => {
-  return {
-    isActive: isSchedulerActive,
-    interval: schedulerInterval ? schedulerInterval._idleTimeout / (60 * 1000) : null,
-  };
-};
+// export const getSchedulerStatus = () => {
+//   return {
+//     isActive: scrapeState.schedulerActive,
+//     interval: schedulerInterval ? schedulerInterval._idleTimeout / (60 * 1000) : null,
+//   };
+// };
