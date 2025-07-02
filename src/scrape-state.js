109078246444f86337f32b3cbd9846fe26feb32f
George Thomas <fake@email.org>
2025-07-02 18:42:45 -0400

    dev

--- src/scrape-state.js
+++ src/scrape-state.js
@@ -1,4 +1,5 @@
 import { textStrMap, runScrapeMap, startStopMap } from "../config/map-scrape.js";
+import dbModel from "../models/db-model.js";
 
 export const scrapeState = {
   scrapeId: null,
@@ -13,11 +14,30 @@ export const scrapeState = {
 
 export const updateScrapeStateByCommand = async (inputType) => {
   if (!inputType) return null;
-
+  const { scrapeId } = scrapeState;
   //set the textStr
   scrapeState.textStr = textStrMap[inputType];
   scrapeState.runScrape = runScrapeMap[inputType];
   startStopMap[inputType]?.();
+
+  //set scrapeId if null
+  if (!scrapeId) {
+    const params = {
+      keyToLookup: "startTime",
+      howMany: 1,
+    };
+
+    const logArray = await dbModel.getLastItemsArray(params);
+    console.log("!!!!!!!logArray");
+    console.log(logArray);
+
+    const scrapeIdLog = logArray[0]._id.toString();
+
+    console.log("****scrapeIdLog");
+    console.log(scrapeIdLog);
+
+    scrapeState.scrapeId = scrapeIdLog;
+  }
 
   return true;
 };
