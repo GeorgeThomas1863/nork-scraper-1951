837fc55f367061aa21e98dccd082e96bb2b4b5e0
George H. Thomas <georgebushdontcare@gmail.com>
2025-07-04 11:42:07 -0400

    still debugging

--- src/clean-fs.js
+++ src/clean-fs.js
@@ -145,11 +145,10 @@ export const reDownloadMedia = async () => {
       console.log(type);
 
       const fileArrayFS = await getFileArrayFS(type);
-
-      console.log("FILE ARRAY FS");
-      console.log(fileArrayFS);
-
       const fileArrayDB = await getFileArrayDB(type);
+
+      console.log("FILE ARRAY DB");
+      console.log(fileArrayDB);
 
       const redownloadArray = await getRedownloadArray(fileArrayFS, fileArrayDB);
       if (!redownloadArray || !redownloadArray.length) continue;
